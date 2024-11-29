8c66fdd95bbfc63b0444dd891ce721cb68411981
Kaiky Federsoni Martins <147348962+kaikyfedersoni@users.noreply.github.com>
2024-11-28 23:17:12 -0300

    Update CarroRequestDTO.java

--- demo/src/main/java/com/example/demo/model/to/CarroRequestDTO.java
+++ demo/src/main/java/com/example/demo/model/to/CarroRequestDTO.java
@@ -1,19 +1,10 @@
 package com.example.demo.model.to;
 
-
-
-<<<<<<< HEAD
-import com.example.demo.model.Marca;
-
-public record CarroRequestDTO(Marca marca, String modelo, int ano,String urlImagem, String combustao, int marchas, double precoDiaria) {
-    
-=======
-
 import com.example.demo.model.Marca;
 
 public record CarroRequestDTO(Marca marca, String modelo, int ano,String urlImagem, String combustao, int marchas, double precoDiaria) {
 
->>>>>>> f12afe8f26cdcf458f78e0031cd5ed7a44d68cec
+
     
-    
+   
 }
