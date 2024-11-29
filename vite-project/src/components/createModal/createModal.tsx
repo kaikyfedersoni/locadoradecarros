b00e16be43ef229141c81623035e4c9d6f044adb
Wesley-Dre <147349167+Wesley-Dre@users.noreply.github.com>
2024-11-28 21:31:13 -0300

    Update createModal.tsx

--- vite-project/src/components/createModal/createModal.tsx
+++ vite-project/src/components/createModal/createModal.tsx
@@ -74,11 +74,6 @@ export function CreateModal({ closeModal }: ModalProps) {
                         <button type="button" onClick={closeModal} className="btn-secondary">Voltar</button>
                     </div>
                 </form>
-<<<<<<< HEAD
-=======
-                <button onClick={submit} className="btn-secondary">salvar</button>
-            
->>>>>>> f12afe8f26cdcf458f78e0031cd5ed7a44d68cec
             </div>
         </div>
     );
