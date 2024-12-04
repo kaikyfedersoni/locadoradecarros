014ff9fd01b8b03c7ce0c06ccd63ff04a25cb26b
Kaiky Federsoni Martins <147348962+kaikyfedersoni@users.noreply.github.com>
2024-12-03 21:42:45 -0300

    Delete vite-project directory

--- vite-project/src/components/editModal/buscarModal.tsx
+++
@@ -1,7 +0,0 @@
-import { useEffect, useState } from "react";
-import { useCarroDataMutate } from "../../hooks/useCarroDataMutate";
-import { CarroData } from "../../interface/CarroData";
-
-
-
-import "./modal.css";
